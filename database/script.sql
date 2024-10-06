db247605303318d6d13ff278298a7864e7b9c3e3
DiegoTinedo <116541647+dtinedor@users.noreply.github.com>
2024-10-06 21:30:48 +0200

    Add files via upload

---
+++ database/script.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE tareas;
+
+use tareas;
+
+CREATE TABLE task(
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DESCRIBE task;
